12e747c8fb0c40edb968caed0929e5a3eba9d1c8
Salimzyanov Lenar <salimzyanov_lenar@mail.ru>
2024-03-18 18:29:58 +0400

    add axios

---
+++ React_app/src/components/Home.js
@@ -0,0 +1,17 @@
+// About.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Home(props) {
+  return (
+
+    <h1> This is our homepage</h1>
+
+  );
+}
+
+Home.propTypes = {
+  window: PropTypes.func,
+};
+
+export default Home;
